dc83be6aeb5c44573c5fb80d9d932d90b6f72aa5
Muhammed Ali Kurt <m.a.kurt53@gmail.com>
2023-04-11 03:13:39 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,5 +1,8 @@
 public class AtmApp {
     public static void main(String[] args) {
+        //version 1
         System.out.println("Bu bir ATM projesidir.");
+        //version 2
+        System.out.println("Java is easy");
     }
 }
